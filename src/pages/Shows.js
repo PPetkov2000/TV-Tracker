41d5dbcd5f6771197b519590d8551e7d1cdc63c8
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2021-03-24 00:02:28 +0200

    add search form

--- src/pages/Shows.js
+++ src/pages/Shows.js
@@ -1,9 +1,9 @@
 import React from "react";
 import useFetch from "../hooks/useFetch";
-import { rootUrl } from "../utils/rootUrl";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Show from "../components/Show";
+import { rootUrl } from "../utils/rootUrl";
 import { showsLimit } from "../utils/itemsLimit";
 import Paginate from "../components/Paginate";
 
@@ -11,6 +11,8 @@ const Shows = ({ match }) => {
   const { loading, data: shows, error } = useFetch(`${rootUrl}/shows`);
   const page = Number(match.params.pageNumber) || 1;
   const pages = shows && Math.ceil(shows.length / showsLimit);
+  const name = match.params.name;
+  const { data: searchedShows } = useFetch(`${rootUrl}/search/shows?q=${name}`);
 
   return (
     <section className="shows">
@@ -22,14 +24,17 @@ const Shows = ({ match }) => {
           <Message variant="red">{error}</Message>
         ) : (
           <div className="shows__list">
-            {shows
-              .slice(showsLimit * (page - 1), showsLimit * page)
-              .map((show) => (
-                <Show key={show.id} show={show} />
-              ))}
+            {!name &&
+              shows
+                .slice(showsLimit * (page - 1), showsLimit * page)
+                .map((show) => <Show key={show.id} show={show} />)}
+            {name &&
+              searchedShows
+                .map((x) => x.show)
+                .map((show) => <Show key={show.id} show={show} />)}
           </div>
         )}
-        <Paginate page={page} pages={pages} />
+        {!name && <Paginate page={page} pages={pages} />}
       </div>
     </section>
   );
