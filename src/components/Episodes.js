855c0d5cd553855e12b30ec977c1365f49f97d65
PPetkov <ppetkov2000@abv.bg>
2022-02-24 01:17:02 +0200

    update dependencies

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -39,3 +40,46 @@ const Episodes = ({ episodes }) => {
 };
 
 export default Episodes;
+=======
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Episodes = ({ episodes }) => {
+  return (
+    <section className="episodes">
+      <div className="content-wrapper">
+        <h2 className="episodes__title">Previous Episodes</h2>
+        <table className="episodes__table">
+          <thead>
+            <tr>
+              <th>Episode</th>
+              <th>Name</th>
+              <th>Airdate</th>
+              <th>Airtime</th>
+              <th>Runtime</th>
+            </tr>
+          </thead>
+          <tbody>
+            {episodes.map((episode) => (
+              <tr key={episode.id}>
+                <td>
+                  {episode.season}x
+                  {episode.number < 10 ? `0${episode.number}` : episode.number}
+                </td>
+                <td>
+                  <Link to={`/episodes/${episode.id}`}>{episode.name}</Link>
+                </td>
+                <td>{episode.airdate}</td>
+                <td>{episode.airtime}</td>
+                <td>{episode.runtime} min</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
+
+export default Episodes;
+>>>>>>> 4a678be... initial commit
--- src/utils/rootUrl.js
+++ src/utils/rootUrl.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export const rootUrl = "http://api.tvmaze.com";
+=======
+export const rootUrl = "http://api.tvmaze.com";
+>>>>>>> 4a678be... initial commit
