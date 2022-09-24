57504a8a1c0e2ce643b1c832aa1b11671d98b71d
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-25 00:20:33 +0300

    Update SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,31 +1,25 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SearchForm = () => {
-  const navigate = useNavigate();
-  const [keyword, setKeyword] = useState("");
+  const navigate = useNavigate()
+  const [keyword, setKeyword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (keyword) {
+      navigate(`/search/shows/${keyword}`)
+    }
+  }
 
   return (
-    <form
-      className="search-form"
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (keyword) {
-          navigate(`/search/shows/${keyword}`);
-        }
-      }}
-    >
-      <input
-        type="text"
-        className="search-form__input"
-        value={keyword}
-        onChange={(e) => setKeyword(e.target.value)}
-      />
+    <form className="search-form" onSubmit={handleSubmit}>
+      <input type="text" className="search-form__input" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
       <button type="submit" className="search-form__button button-primary">
         Search
       </button>
     </form>
-  );
-};
+  )
+}
 
-export default SearchForm;
+export default SearchForm
