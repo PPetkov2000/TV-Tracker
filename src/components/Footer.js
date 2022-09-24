ef99eb35b1f6d4ee83924204018582ae6dad10dd
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2022-09-25 00:25:58 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="content-wrapper">
-        <h3 className="footer__text">&copy; TV Tracker 2021</h3>
+        <h3 className="footer__text">&copy; TV Tracker {new Date().getFullYear().toString()}</h3>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
