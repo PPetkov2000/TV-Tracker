3c354d826261895ccf6cd0ccf2930c488cfef0fa
Plamen <34969110+PPetkov2000@users.noreply.github.com>
2023-07-16 00:05:26 +0300

    Update Show.js

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,21 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { defaultImage } from "../utils/defaultImage";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { defaultImage } from '../utils/defaultImage'
 
 const Show = ({ show }) => {
   return (
     <div className="show">
-      <Link to={`/shows/${show.id}`}>
-        <img
-          src={show.image ? show.image.medium : defaultImage}
-          alt={show.name}
-          className="show__image"
-        />
+      <Link to={`/shows/${show.id}`} aria-label="See show details">
+        <img src={show.image ? show.image.medium : defaultImage} alt={show.name} className="show__image" loading="lazy" />
       </Link>
       <h4 className="show__title">{show.name}</h4>
-      <p className="show__genres">{show.genres.join(", ")}</p>
+      <p className="show__genres">{show.genres.join(', ')}</p>
     </div>
-  );
-};
+  )
+}
 
-export default Show;
+export default Show
