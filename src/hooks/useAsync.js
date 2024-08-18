01d712de776f034e8bd442d2bbfa19d2b9817e12
PPetkov <ppetkov2000@abv.bg>
2024-08-18 13:38:02 +0300

    update useAsync hook

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -5,23 +5,26 @@ const useAsync = (func, dependencies = [], immediate = true) => {
   const [error, setError] = useState()
   const [data, setData] = useState()
 
-  const execute = useCallback((...params) => {
-    setLoading(true)
-    return func(...params)
-      .then(({ data }) => {
-        setData(data)
-        setError(undefined)
-        return data
-      })
-      .catch((error) => {
-        setError(error)
-        setData(undefined)
-        return Promise.reject(error)
-      })
-      .finally(() => {
-        setLoading(false)
-      })
-  }, dependencies)
+  const execute = useCallback(
+    (...params) => {
+      setLoading(true)
+      return func(...params)
+        .then(({ data }) => {
+          setData(data)
+          setError(undefined)
+          return data
+        })
+        .catch((error) => {
+          setError(error)
+          setData(undefined)
+          return Promise.reject(error)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    },
+    [...dependencies, func]
+  )
 
   useEffect(() => {
     if (immediate) {
